72f216fc0080a2a27ffe954834075821c1ab3e54
mingorancejuan5 <mingorancejuan5@gmail.com>
2024-05-20 20:32:06 +0000

    notty initial commit

---
+++ src/app.tsx
@@ -0,0 +1,20 @@
+
+
+
+function App(){
+
+
+return (
+
+
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:3677543890.
+  <div>
+    <h1>Hello</h1>
+  </div>
+
+)
+
+
+}
+
+export default App
